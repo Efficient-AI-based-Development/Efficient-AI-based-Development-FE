4a22372cbd93e986dacf9a406b116410197afead
parkjihyoun <jihyoun2002@knu.ac.kr>
2025-10-10 17:36:17 +0900

    chore: ESLint+Prettier 기본 규칙 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint","react","react-hooks","import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  settings: { react: { version: "detect" } }
+};
