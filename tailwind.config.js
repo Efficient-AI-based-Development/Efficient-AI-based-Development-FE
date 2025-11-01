c89422d2635108a75f4edab7f6322f65ba1d58db
parkjihyoun <jihyoun2002@knu.ac.kr>
2025-11-01 18:35:38 +0900

    AD-1 feat: Tailwind 커스텀 색상 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{ts,tsx}"],
-    theme: { extend: {} },
+    theme: {
+        extend: {
+            colors: {
+                primary: "#7871FE",
+                secondary: "#4C2EFF",
+                customBlack: "#1E1E1E",
+            },
+        },
+    },
     plugins: [],
 };
